206dc080e56d3f6dd74baee2be669db8c903d963
Dias <example@gmail.com>
2022-12-05 17:23:55 +0600

    lesson19

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const numberOfFilms = +prompt("Сколько фильмов вы посмотрели?","");
-console.log(numberOfFilms);
+// const numberOfFilms = +prompt("Сколько фильмов вы посмотрели?","");
+// console.log(numberOfFilms);
 
 const personalMovieDB = {
     count:numberOfFilms,
@@ -8,15 +8,29 @@ const personalMovieDB = {
     genres:[],
     privat:false
 };
-const a = prompt('Один из последних посмотренных фильмов?','');
-const b = prompt('На сколько оцените ее?','');
-const c = prompt('Один из последних посмотренных фильмов?','');
-const d = prompt('На сколько оцените ее?','');
+// const a = prompt('Один из последних посмотренных фильмов?','');
+// const b = prompt('На сколько оцените ее?','');
+// const c = prompt('Один из последних посмотренных фильмов?','');
+// const d = prompt('На сколько оцените ее?','');
 // personalMovieDB.movies = {
 //     a:b,
 //     c:d
 // };
 
-personalMovieDB.movies[a]=b;
-personalMovieDB.movies[c]=d;
+// personalMovieDB.movies[a]=b;
+// personalMovieDB.movies[c]=d;
+// console.log(personalMovieDB);
+
+for(let i=0;i<2;i++){
+    const a = prompt('Один из последних посмотренных фильмов?','');
+    const b = prompt('На сколько оцените ее?','');
+    if(a!=null && b!=null && a!='' && b!='' && a.length<50){
+        personalMovieDB.movies[a]=b;
+        console.log("done");
+    }else{
+        console.log('error');
+        i--;
+    }
+
+}
 console.log(personalMovieDB);

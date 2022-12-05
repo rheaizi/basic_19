27e8c61b9e7c25d7335b2e5f1b9a16f45932e1f2
Dias <example@gmail.com>
2022-12-05 17:45:42 +0600

    lesson25

--- script.js
+++ script.js
@@ -1,36 +1,35 @@
-// const numberOfFilms = +prompt("Сколько фильмов вы посмотрели?","");
-// console.log(numberOfFilms);
-
-const personalMovieDB = {
-    count:numberOfFilms,
-    movies:{},
-    actors:{},
-    genres:[],
-    privat:false
-};
-// const a = prompt('Один из последних посмотренных фильмов?','');
-// const b = prompt('На сколько оцените ее?','');
-// const c = prompt('Один из последних посмотренных фильмов?','');
-// const d = prompt('На сколько оцените ее?','');
-// personalMovieDB.movies = {
-//     a:b,
-//     c:d
-// };
-
-// personalMovieDB.movies[a]=b;
-// personalMovieDB.movies[c]=d;
-// console.log(personalMovieDB);
-
-for(let i=0;i<2;i++){
-    const a = prompt('Один из последних посмотренных фильмов?','');
-    const b = prompt('На сколько оцените ее?','');
-    if(a!=null && b!=null && a!='' && b!='' && a.length<50){
-        personalMovieDB.movies[a]=b;
-        console.log("done");
-    }else{
-        console.log('error');
-        i--;
-    }
-
+"use strict";
+let num=20;
+function showFirstMessage(text){
+    console.log(text);
+    console.log(num);
+    
 }
-console.log(personalMovieDB);
+
+showFirstMessage("Hello World");
+console.log(num);
+
+
+
+console.log(calc(4,3));
+console.log(calc(4,6));
+console.log(calc(10,3));
+
+function calc(a,b){
+    return (a+b);
+}
+
+function ret(){
+    let num=50;
+    return num;
+}
+const anotherNum = ret();
+console.log(anotherNum);
+
+const logger = function(){
+    console.log("Hello");
+}
+logger();
+
+const calculator1 = (a,b) => {return a+b;};
+const calculator2 = (a,b) => a+b;
